90ad730859e0d4b4d0acb408701a060a8a49b763
Weerawut1996 <wirawoot789@hotmail.com>
2023-09-08 16:16:59 +0700

    ! chage section tag

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,28 @@ import { useEffect, useState } from 'react';
 function App() {
    const [pageState, setpageState] = useState('A1 connect table')
    const [countchage, setcountchage] = useState(0)
-   
+
    useEffect(() => {
       console.log(pageState);
       // console.log(countchage);
    }, [countchage])
-   
-   const refreshTable = () =>{
+
+   const refreshTable = () => {
       setcountchage(prevValue => prevValue === 0 ? 1 : 0)
       setpageState('A1 refresh table')
    }
 
+
    return (
       <>
          <header className='header'>
-            <h1>Vite + React, axios</h1>
-            <h2>Create, Read, Update, Delete</h2>
-            <hr />
+            <h1>Vite + React, axios (CRUD)</h1>
+            <p>Create, Read, Update, Delete</p>
          </header>
-         <section className='local-Timer'>
-            <DateAndTime />
-         </section>
-         <section className='create-form'>
-            <CreateSheets/>
-         </section>
-         <section className='read-table'>
+         <DateAndTime />
+         <hr />
+         <CreateSheets />
+         <section className='read-table t-ali-cen'>
             <button className='f-buttom' onClick={refreshTable}>Refresh</button>
             <ReadSheets key={countchage} />
          </section>
