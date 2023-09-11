750255df6dcbe54805dd60ea04b686dc5a1dfcb6
Weerawut1996 <wirawoot789@hotmail.com>
2023-09-11 16:25:56 +0700

    ! chage data formet

--- src/Component/DateAndTime.jsx
+++ src/Component/DateAndTime.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
+import { convertDate } from '../Function/PackFunction';
 
 const DateAndTime = () => {
-   const day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+   const day = ['อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์']
    const [time_now, settime_now] = useState(new Date().toLocaleTimeString())
    useEffect(() => {
       const time_interval = setInterval(() => { settime_now((time_now) => time_now = new Date().toLocaleTimeString()) }, 1000);
@@ -10,10 +11,10 @@ const DateAndTime = () => {
    return (
       <section className='local-Timer'>
          <div>
-            <h3>Today :</h3> <p>{day[new Date().getDay()]} {new Date().toLocaleDateString()}</p>
+            <h3>วันนี้ :</h3>&#32;<p>วัน{day[new Date().getDay()]} {convertDate(new Date().toLocaleDateString())}</p>
          </div>
          <div>
-            <h3>Time :</h3> <p>{time_now}</p>
+            <h3>เวลา :</h3>&#32;<p>{time_now}</p>
          </div>
       </section>
    )
