82e9b1302340af9cd038404bca7324b2bff19d04
Weerawut1996 <wirawoot789@hotmail.com>
2023-09-11 16:27:09 +0700

    ! new fn

--- src/Function/PackFunction.js
+++ src/Function/PackFunction.js
@@ -1,14 +1,28 @@
 export function convertDate(inputDate) {
    // แยกวันที่, เดือน และปี
    var parts = inputDate.split('/');
+   // console.log(parts);
    var day = parts[0];
    var month = parts[1];
    var year = parts[2];
-   // สร้างรายการของชื่อเดือน
-   var monthNames = [ "ม.ค", "ก.พ", "มี.ค", "เม.ย", "พ.ค", "มิ.ย", "ก.ค", "ส.ค", "ก.ย", "ต.ค", "พ.ย", "ธ.ค"];
+   // รายการของชื่อเดือน
+   var monthNames = ["ม.ค", "ก.พ", "มี.ค", "เม.ย", "พ.ค", "มิ.ย", "ก.ค", "ส.ค", "ก.ย", "ต.ค", "พ.ย", "ธ.ค"];
    // แปลงเดือนเป็นชื่อเดือน
    var monthName = monthNames[parseInt(month) - 1];
-   // สร้างวันที่ในรูปแบบที่ต้องการ
+   // สร้างรูปแบบวันที่
    var result = day + " " + monthName + " " + year;
    return result;
 }
+
+export function TelephoneFormat(input) {
+   if (!isNaN(input.length - 1)) {
+      console.log(input.length - 1);
+      return input.slice(0, -1);
+   } else {
+      
+   }
+   const lastCharacter = input.charAt();
+}
+
+
+
