93f180fcd79af8efdf50d92868a74dd0c311b5b7
Weerawut1996 <wirawoot789@hotmail.com>
2023-09-15 16:22:56 +0700

    ! new css

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
+import './App400.css'
 import './Css/class.css'
 import './Css/id.css'
 import './index.css'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>,)
