9d1a720a48f30a9bf9c0d0c89dd979e59e23db50
Weerawut1996 <wirawoot789@hotmail.com>
2023-09-15 16:23:23 +0700

    ! more comment

--- src/Component/A1_Create.jsx
+++ src/Component/A1_Create.jsx
@@ -1,37 +1,35 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { TelephoneFormat, filterSymbols, RandomId, convertDate, reformatInputDate } from '../Function/PackFunction';
+import { TelephoneFormat, filterSymbols, RandomId, convertDate } from '../Function/PackFunction';
 
 
 
 
 const CreateSheets = (props) => {
+   // use in 'app.jsx'
    const [Prefix, setPrefix] = useState('0')
    const [Rank, setRank] = useState('0')
    const [First_name, setFirst_name] = useState('')
    const [Last_name, setLast_name] = useState('')
    const [gender, setgender] = useState('')
-   const [Birth_date, setBirth_date] = useState('')
+   const [Birth_date, setBirth_date] = useState('2000-01-01')
    const [Telephone, setTelephone] = useState('')
    const [Email, setEmail] = useState('')
    // console.log('มีการ log บรรทัดนี้ทุกครั้ง ที่มาการ ป้อนข้อมูลลง tag input');
-
-   const updateTable = () => {      
-      props.addsomenew();
-    };
-
+   const today = new Date().toLocaleDateString('th-TH')
+   const updateTable = () => { props.addsomenew(); };
 
    function add(e) {
-      e.preventDefault() // e.preventDefault() ไว้ป้องกัน event web refed เมื่อกด supmit จาก form      
+      e.preventDefault() // e.preventDefault() ไว้ป้องกัน event web refed เมื่อกด supmit จาก form
+      const newPrefix = Prefix === "1" ? 'นาย' : Prefix === "2" || Prefix === "3" ? 'นาง' : 'ไม่ระบุ';
+      const newRank_id = Rank === "1" ? 'ABC' : Rank === "2" ? 'D45' : Rank === "3" ? 'XYZ' : Rank === "4" ? 'PYE' : '000';
+      const newRank = Rank === "1" ? 'ระดับ 1 ABC' : Rank === "2" ? 'ระดับ 2 D45' : Rank === "3" ? 'ระดับ 3 XYZ' : '000';
+      const newgender = gender === "1" ? 'ชาย' : gender === "2" ? 'หญิง' : 'ไม่ระบุ';
+      const nowBirth_date = convertDate(new Date(Birth_date).toLocaleDateString('th-TH'))
+      // console.log(`${newRank_id}-${newEmployee_id}`, newPrefix, First_name, Last_name, newgender);
+      // console.log(nowBirth_date, Telephone, Email);
       const newEmployee_id = RandomId(8)
       // loop check id ซ้ำ
-      const newPrefix = Prefix === "1" ? 'นาย' : Prefix === "2" || Prefix === "3" ? 'นาง' : 'ไม่ระบุ';      
-      const newRank_id = Rank === "1" ? 'ABC' : Rank === "2" ? 'D45' : Rank === "3" ? 'XYZ' : '---';
-      const newRank = Rank === "1" ? 'ระดับ 1 ABC' : Rank === "2" ? 'ระดับ 2 D45' : Rank === "3" ? 'ระดับ 3 XYZ' : '---';
-      const newgender = gender === "1" ? 'ชาย' : Rank === "2" ? 'หญิง' : 'ไม่ระบุ';
-      const nowtime = `${convertDate(new Date().toLocaleDateString())} ${new Date().toLocaleTimeString().substring(0, 5)}`
-      const nowBirth_date = reformatInputDate(Birth_date)
-      // console.log(`${newRank_id}-${newEmployee_id}`, newPrefix, First_name, Last_name, newgender, nowBirth_date, Telephone, Email, nowtime);
       const newData = {
          sheets1: {
             employeeId: `${newRank_id}-${newEmployee_id}`,
@@ -43,10 +41,9 @@ const CreateSheets = (props) => {
             thaiBirthDate: nowBirth_date,
             telephone: Telephone,
             email: Email,
-            addTimeStamp: nowtime,
+            addTimeStamp: `${convertDate(today)} ${new Date().toLocaleTimeString().substring(0, 5)}`
          }
-       };
-      
+      };
       axios.post('https://api.sheety.co/4ca9ed09b8eddce654c9316dcee071de/addData/sheets1', newData)
          .then(function (response) {
             console.log(response);
@@ -64,81 +61,78 @@ const CreateSheets = (props) => {
    }, [Prefix])
 
    return (
-      <>
-         <section className='create-form t-ali-cen'>
-            <form onSubmit={add} className='add-form'>
-               <label>คำนำหน้า :</label>
-               <select name="Prefix" onChange={(e) => setPrefix(e.target.value)} value={Prefix}>
-                  <option value="0">...คำนำหน้า...</option>
-                  <option value="1">นาย</option>
-                  <option value="2">นาง</option>
-                  <option value="3">นางสาว</option>
-               </select>
-               <label>ตำแหน่ง :</label>
-               <select name="NewRank" onChange={(e) => setRank(e.target.value)} value={Rank}>
-                  <option value="0">...ตำแหน่ง...</option>
-                  <option value="1">ตำแหน่ง ABC</option>
-                  <option value="2">ตำแหน่ง D45</option>
-                  <option value="3">ตำแหน่ง XYZ</option>
-               </select>
-               <label>ชื่อ :</label>
-               <input
-                  type="search"
-                  placeholder="First name"
-                  value={First_name} required
-                  onChange={(e) => setFirst_name(filterSymbols('name', e.target.value))} />
-               {/* ใช้ useState จึงไม่จำเป็นต้องกำหนด id ใน input tag */}
-               <label>นามสกุล : </label>
-               <input
-                  type="search"
-                  placeholder="Last name"
-                  value={Last_name} required
-                  onChange={(e) => setLast_name(filterSymbols('name', e.target.value))} />
-               <label>เพศ : </label>
-               <select name="Gender" onChange={(e) => setgender(e.target.value)} value={gender} required>
-                  <option value="0">...ระบุเพศ...</option>
-                  <option value="1">ชาย</option>
-                  <option value="2">หญิง</option>
-               </select>
-               <label>วันเกิด : </label>
-               <input
-                  type="date"
-                  placeholder="วว / ดด / ค.ศ"
-                  value={Birth_date} required
-                  onChange={(e) => setBirth_date(e.target.value)} />
-               <label>เบอร์โทรศัพท์ : </label>
-               <input
-                  type="search"
-                  placeholder="Telephone"
-                  maxLength="16" size="16"
-                  value={Telephone} required
-                  onChange={(e) => setTelephone(TelephoneFormat(e.target.value))} />
-               <label>Email : </label>
-               <input
-                  type="email"
-                  placeholder="Email"
-                  value={Email} required
-                  onChange={(e) => setEmail(filterSymbols('email', e.target.value))} />
-               <br />
-               <div className="rebuttom ">
-                  <input type="reset" value="Reset"
-                     onClick={() => {
-                        console.log('reset');
-                        setPrefix('0');
-                        setRank('0');
-                        setFirst_name('');
-                        setLast_name('');
-                        setBirth_date('');
-                        setTelephone('');
-                        setEmail('')
-                     }} />
-                  <input type="submit" value="เพิ่มข้อมูล" />
-               </div>
-            </form>
-
-         </section>
-
-      </>
+      <section className='create-form t-ali-cen'>
+         <form onSubmit={add} className='add-form'>
+            <label>คำนำหน้า :</label>
+            <select name="Prefix" onChange={(e) => setPrefix(e.target.value)} value={Prefix}>
+               <option value="0">...คำนำหน้า...</option>
+               <option value="1">นาย</option>
+               <option value="2">นาง</option>
+               <option value="3">นางสาว</option>
+            </select>
+            <label>ตำแหน่ง :</label>
+            <select name="NewRank" onChange={(e) => setRank(e.target.value)} value={Rank}>
+               <option value="0">...ตำแหน่ง...</option>
+               <option value="1">ตำแหน่ง ABC</option>
+               <option value="2">ตำแหน่ง D45</option>
+               <option value="3">ตำแหน่ง XYZ</option>
+               <option value="4">ตำแหน่ง PYE</option>
+            </select>
+            <label>ชื่อ :</label>
+            <input
+               type="search"
+               placeholder="First name"
+               value={First_name} required
+               onChange={(e) => setFirst_name(filterSymbols('name', e.target.value))} />
+            {/* ใช้ useState จึงไม่จำเป็นต้องกำหนด id ใน input tag */}
+            <label>นามสกุล : </label>
+            <input
+               type="search"
+               placeholder="Last name"
+               value={Last_name} required
+               onChange={(e) => setLast_name(filterSymbols('name', e.target.value))} />
+            <label>เพศ : </label>
+            <select name="Gender" onChange={(e) => setgender(e.target.value)} value={gender} required>
+               <option value="0">...ระบุเพศ...</option>
+               <option value="1">ชาย</option>
+               <option value="2">หญิง</option>
+            </select>
+            <label>วันเกิด : </label>
+            <input
+               type="date"
+               placeholder="วว / ดด / ค.ศ"
+               value={Birth_date} required
+               onChange={(e) => setBirth_date(e.target.value)} />
+            <label>เบอร์โทรศัพท์ : </label>
+            <input
+               type="search"
+               placeholder="Telephone"
+               maxLength="16" size="16"
+               value={Telephone} required
+               onChange={(e) => setTelephone(TelephoneFormat(e.target.value))} />
+            <label>Email : </label>
+            <input
+               type="email"
+               placeholder="Email"
+               value={Email} required
+               onChange={(e) => setEmail(filterSymbols('email', e.target.value))} />
+            <br />
+            <div className="rebuttom ">
+               <input type="reset" value="Reset"
+                  onClick={() => {
+                     console.log('reset');
+                     setPrefix('0');
+                     setRank('0');
+                     setFirst_name('');
+                     setLast_name('');
+                     setBirth_date('');
+                     setTelephone('');
+                     setEmail('')
+                  }} />
+               <input type="submit" value="เพิ่มข้อมูล" />
+            </div>
+         </form>
+      </section>
    );
 }
 
--- src/Component/A2_Read.jsx
+++ src/Component/A2_Read.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
+import { CalculateAge } from '../Function/PackFunction';
 
 function ReadSheets() {
+   // use in 'app.jsx'
    const [displaydata, setdisplaydata] = useState([]);// set type array
    const [StatusCode, setStatusCode] = useState(<>On Load.....</>);
-   const [chageCount, setchageCount] = useState(0);
-
    const sheetsfetch = async () => {
-      const sheetsURL = 'https://api.sheety.co/4ca9ed09b8eddce654c9316dcee071de---/addData/sheets1';
+      const sheetsURL = 'https://api.sheety.co/4ca9ed09b8eddce654c9316dcee071de/addData/sheets1';
       await axios.get(sheetsURL)
          .then(response => {
             // console.log(response);
+            console.log('%c...','background: green; color: green;','fatch ok and complete');
             setStatusCode(<>Status Code: {response.status}</>)
-            console.log(response.data)
+            console.log()
             const resdata = response.data.sheets1
             setdisplaydata(resdata?.map((val, i) => (
                //loop map จำเป็นต้องมี key
@@ -28,7 +29,8 @@ function ReadSheets() {
                   </td>
                   <td>{val.physicalGender}</td>
                   <td>{val.thaiBirthDate}</td>
-                  <td>{val.age}</td>
+
+                  <td>{CalculateAge(val.thaiBirthDate) < 0 ? '???' : CalculateAge(val.thaiBirthDate)}</td>
                   <td>{val.telephone}</td>
                   <td>{val.email}</td>
                   <td>{val.addTimeStamp}</td>
@@ -42,7 +44,7 @@ function ReadSheets() {
             if (error.response) {
                const sheetyUpgrade = <a href="https://dashboard.sheety.co/upgrade" target="_blank" rel="noopener noreferrer">sheety upgrade</a>
                // มี response กลับมาแต่ status code เป็น error
-               if (error.response.status == 402 || error.response.status == 500) {
+               if (error.response.status == 402) {
                   const sheetLink = <a href={sheetyUpgrade} target='_blank'>{sheetyUpgrade}</a>
                   setStatusCode(<>Status Code: {error.response.status}<br />This Free Account Over Requests (Max 200 per/Month)<br />{sheetLink}</>);
                }
@@ -61,7 +63,7 @@ function ReadSheets() {
    }
    useEffect(() => {
       sheetsfetch()
-   }, [chageCount])
+   }, [])
    return (
       <table>
          <thead>
@@ -79,6 +81,8 @@ function ReadSheets() {
          </thead>
          <tbody>
             <tr><td colSpan={9}><p className='css-fix t-ali-cen'>{StatusCode}</p></td></tr>
+            <tr className="sheets-data-format"><td>ABC-E0Q998FA</td><td>ระดับ 1 ABC</td><td><p className="fullname"><span>นาย</span><span>ธาวัน</span><span>นันทพินิจ</span></p></td><td>ชาย</td><td>15/07/2540</td><td>26 ปี</td><td>090-095-1255</td><td>Waaaa123456@hhhhhhh.com</td><td>12 ก.ย 2566 15:34</td></tr>
+            <tr className="sheets-data-format"><td>D45-QJC7F61H</td><td>ระดับ 2 D45</td><td><p className="fullname"><span>นาง</span><span>ณิชา</span><span>วัฒนศักดิ์สกุล</span></p></td><td>หญิง</td><td>20/05/2539</td><td>27 ปี</td><td>084-555-4444</td><td>nizcha478@hGGG.com</td><td>13 ก.ย 2566 15:48</td></tr>
             {displaydata}
          </tbody>
       </table>
