922cafa30300d86336558bcf0881c9d45f838fca
Weerawut1996 <wirawoot789@hotmail.com>
2023-09-15 16:24:30 +0700

    ! add toLocaleDateString

--- src/Component/DateAndTime.jsx
+++ src/Component/DateAndTime.jsx
@@ -3,15 +3,15 @@ import { convertDate } from '../Function/PackFunction';
 
 const DateAndTime = () => {
    const day = ['อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์']
-   const [time_now, settime_now] = useState(new Date().toLocaleTimeString())
+   const [time_now, settime_now] = useState(new Date().toLocaleTimeString('th-TH'))
    useEffect(() => {
-      const time_interval = setInterval(() => { settime_now((time_now) => time_now = new Date().toLocaleTimeString()) }, 1000);
+      const time_interval = setInterval(() => { settime_now((time_now) => time_now = new Date().toLocaleTimeString('th-TH')) }, 1000);
       return () => clearInterval(time_interval);
    }, []);
    return (
       <section className='local-Timer'>
          <div>
-            <h3>วันนี้ :</h3>&#32;<p>วัน{day[new Date().getDay()]} {convertDate(new Date().toLocaleDateString())}</p>
+            <h3>วันนี้ :</h3>&#32;<p>วัน{day[new Date().getDay()]} {convertDate(new Date().toLocaleDateString('th-TH'))}</p>
          </div>
          <div>
             <h3>เวลา :</h3>&#32;<p>{time_now}</p>
