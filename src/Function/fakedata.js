57eafa1018f8a21f4d49a49710d393f4870c0eac
Weerawut1996 <wirawoot789@hotmail.com>
2023-09-20 16:32:30 +0700

    ! add fake data file

---
+++ src/Function/fakedata.js
@@ -0,0 +1,27 @@
+export const fakedata_1 ={
+   "employeeId": "Test-ABC-E0Q998FA",
+   "rank": "ระดับ 222 ABC",
+   "prefix": "นาย",
+   "firstName": "ณิ55ชา",
+   "lastName": "ศักดิ์สกุล",
+   "physicalGender": "ชาย",
+   "thaiBirthDate": "16 มิ.ย. 2537",
+   "telephone": "084-183-2542",
+   "email": "nicha_2537@hotmail.com",
+   "addTimeStamp": "15 ก.ย 2566 15:50",
+   "id": "E0Q998FA"
+}
+
+export const fakedata_2 ={
+   "employeeId": "Test-D45-QJC7F61H",
+   "rank": "ระดับ 111 ABC",
+   "prefix": "นางสาว",
+   "firstName": "ณิชาQJ",
+   "lastName": "วัฒนQJ",
+   "physicalGender": "หญิง",
+   "thaiBirthDate": "16 มิ.ย. 2505",
+   "telephone": "084-183-9999",
+   "email": "nicha_9999@hotmail.com",
+   "addTimeStamp": "15 ก.ย 2566 15:50",
+   "id": "QJC7F61H"
+}
