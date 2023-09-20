baf8302c9ce99a3bcf5e3530dccd34f371915998
Weerawut1996 <wirawoot789@hotmail.com>
2023-09-20 16:29:50 +0700

    ! add delete fn

---
+++ src/Component/A3_Delete.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const DeleteSheet = async (id) => {
+      //ได้ใช้
+      console.log('del', id);
+      if (id === 'Test-ABC-E0Q998FA' || id === 'Test-D45-QJC7F61H') {
+         alert(`ห้ามลมหรือแก้ id : ${id}`)
+         return;
+      }
+      //del
+      try {
+         const response = await axios.delete('https://api.sheety.co/4ca9ed09b8eddce654c9316dcee071de/addData/sheets1/' + id);
+         console.log(response);// รอ const response
+         updateTable();
+      } catch (error) {
+         console.log('fail to delete');// รอ const response
+         console.log(error)
+         return error;
+      }
+
+   }
+
+export default DeleteSheet;
+
